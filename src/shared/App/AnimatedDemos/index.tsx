371f4d071f043d814ef543fe9678cd2accffb9b8
jakearchibald <jaffathecake@gmail.com>
2023-03-14 13:40:08 +0000

    Add duration control

--- src/shared/App/AnimatedDemos/index.tsx
+++ src/shared/App/AnimatedDemos/index.tsx
@@ -1,4 +1,9 @@
-import { Signal, useComputed, useSignalEffect } from '@preact/signals';
+import {
+  Signal,
+  useComputed,
+  useSignal,
+  useSignalEffect,
+} from '@preact/signals';
 import { h, Fragment, RenderableProps, FunctionComponent } from 'preact';
 import 'add-css:./styles.module.css';
 import * as styles from './styles.module.css';
@@ -9,18 +14,14 @@ interface Props {
   slightlyOptimizedLinear: Signal<string[]>;
 }
 
-const duration = 2000;
 const gap = 500;
-const animOpts = {
-  duration,
-  fill: 'forwards',
-} as const;
 
 const anim = (
   el: HTMLElement,
   easing: string,
+  duration: number,
   keyframes: Keyframe[] | PropertyIndexedKeyframes,
-) => el.animate(keyframes, { ...animOpts, easing }).finished;
+) => el.animate(keyframes, { fill: 'forwards', duration, easing }).finished;
 
 const useLinearValue = (linear: Signal<string[]>) =>
   useComputed(() =>
@@ -31,12 +32,15 @@ const Demos: FunctionComponent<Props> = ({
   slightlyOptimizedLinear,
   linear,
 }: RenderableProps<Props>) => {
+  const durationInputValue = useSignal('2000');
+  const duration = useComputed(() => Number(durationInputValue.value) || 2000);
   const slightlyOptimizedLinearStr = useLinearValue(slightlyOptimizedLinear);
   const linearStr = useLinearValue(linear);
   const demos = useRef<HTMLDivElement>(null);
   const slightlyOptimizedTranslateEl = useRef<HTMLDivElement>(null);
   const translateEl = useRef<HTMLDivElement>(null);
 
+  // Initial setup
   useEffect(() => {
     let stop = false;
 
@@ -46,11 +50,12 @@ const Demos: FunctionComponent<Props> = ({
           anim(
             slightlyOptimizedTranslateEl.current!,
             slightlyOptimizedLinearStr.value,
+            duration.value,
             {
               transform: ['translateX(0)', 'translateX(100%)'],
             },
           ),
-          anim(translateEl.current!, linearStr.value, {
+          anim(translateEl.current!, linearStr.value, duration.value, {
             transform: ['translateX(0)', 'translateX(100%)'],
           }),
         ]);
@@ -63,11 +68,12 @@ const Demos: FunctionComponent<Props> = ({
           anim(
             slightlyOptimizedTranslateEl.current!,
             slightlyOptimizedLinearStr.value,
+            duration.value,
             {
               transform: ['translateX(100%)', 'translateX(0)'],
             },
           ),
-          anim(translateEl.current!, linearStr.value, {
+          anim(translateEl.current!, linearStr.value, duration.value, {
             transform: ['translateX(100%)', 'translateX(0)'],
           }),
         ]);
@@ -79,13 +85,16 @@ const Demos: FunctionComponent<Props> = ({
     return () => (stop = true);
   }, []);
 
+  // Updates to easing
   useSignalEffect(() => {
-    if (slightlyOptimizedLinearStr.value === '' || linearStr.value === '')
+    if (slightlyOptimizedLinearStr.value === '' || linearStr.value === '') {
       return;
+    }
 
     const slightly = [slightlyOptimizedTranslateEl.current!].flatMap((el) =>
       el.getAnimations(),
     );
+
     const optim = [translateEl.current!].flatMap((el) => el.getAnimations());
 
     for (const anim of slightly) {
@@ -97,17 +106,50 @@ const Demos: FunctionComponent<Props> = ({
     }
   });
 
+  // Updates to duration
+  useSignalEffect(() => {
+    if (slightlyOptimizedLinearStr.value === '' || linearStr.value === '') {
+      return;
+    }
+
+    const anims = [
+      slightlyOptimizedTranslateEl.current!,
+      translateEl.current!,
+    ].flatMap((el) => el.getAnimations());
+
+    for (const anim of anims) {
+      anim.effect!.updateTiming({ duration: duration.value });
+    }
+  });
+
   return (
-    <div ref={demos} class={styles.demos}>
-      <div class={styles.translate}>
-        <div ref={slightlyOptimizedTranslateEl}>
-          <div class={styles.demoBox}>Original</div>
-        </div>
-        <div ref={translateEl}>
-          <div class={styles.demoBox}>Optimized</div>
+    <>
+      <p>
+        <label>
+          Duration:{' '}
+          <input
+            type="number"
+            value={durationInputValue}
+            onInput={(event) =>
+              (durationInputValue.value = (
+                event.target as HTMLInputElement
+              ).value)
+            }
+          />
+          ms
+        </label>
+      </p>
+      <div ref={demos} class={styles.demos}>
+        <div class={styles.translate}>
+          <div ref={slightlyOptimizedTranslateEl}>
+            <div class={styles.demoBox}>Original</div>
+          </div>
+          <div ref={translateEl}>
+            <div class={styles.demoBox}>Optimized</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
