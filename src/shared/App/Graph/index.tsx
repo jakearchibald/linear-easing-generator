f8528582b517121d4175206ef7f0d7d90a1498ce
jakearchibald <jaffathecake@gmail.com>
2023-04-11 14:21:45 +0100

    Improved graph visual

--- src/shared/App/Graph/index.tsx
+++ src/shared/App/Graph/index.tsx
@@ -94,11 +94,113 @@ const Graph: FunctionComponent<Props> = ({
   const fullPointsPath = useToPath(fullPoints);
   const optimizedPath = useToPath(optimizedPoints);
   const graphSVGRef = useRef<SVGSVGElement>(null);
-  const idealScale = useIdealSVGScale(graphSVGRef, optimizedPoints);
+  const containerSize: Signal<[width: number, height: number] | null> =
+    useSignal(null);
 
-  const graphSvgStyle = useComputed(
-    () => `transform: scale(${idealScale.value})`,
+  useLayoutEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      const size = entries[0].contentBoxSize[0];
+      containerSize.value = [size.inlineSize, size.blockSize];
+    });
+
+    observer.observe(graphSVGRef.current!);
+
+    return () => observer.disconnect();
+  }, []);
+
+  const valueBounds = useComputed(() => {
+    const bounds = { x1: 0, y1: 0, x2: 1, y2: 1 };
+
+    if (optimizedPoints.value === null) return bounds;
+
+    for (const point of optimizedPoints.value) {
+      if (point[0] < bounds.x1) {
+        bounds.x1 = point[0];
+      } else if (point[0] > bounds.x2) {
+        bounds.x2 = point[0];
+      }
+
+      const val = 1 - point[1];
+
+      if (val < bounds.y1) {
+        bounds.y1 = val;
+      } else if (val > bounds.y2) {
+        bounds.y2 = val;
+      }
+    }
+
+    return bounds;
+  });
+
+  const canvasBounds = useComputed(() => {
+    const padding = 60;
+    if (!containerSize.value) return { scale: 1, x1: 0, x2: 1, y1: 0, y2: 1 };
+
+    const canvasUsableWidth = containerSize.value[0] - padding * 2;
+    const canvasUsableHeight = containerSize.value[1] - padding * 2;
+    const boundsWidth = valueBounds.value.x2 - valueBounds.value.x1;
+    const boundsHeight = valueBounds.value.y2 - valueBounds.value.y1;
+
+    let scale: number;
+
+    if (boundsWidth / boundsHeight < canvasUsableWidth / canvasUsableHeight) {
+      scale = canvasUsableHeight / boundsHeight;
+    } else {
+      scale = canvasUsableWidth / boundsWidth;
+    }
+
+    const canvasWidthToScale = containerSize.value[0] / scale;
+    const canvasHeightToScale = containerSize.value[1] / scale;
+    const x1 = valueBounds.value.x1 - (canvasWidthToScale - boundsWidth) / 2;
+    const x2 = x1 + canvasWidthToScale;
+    const y1 = valueBounds.value.y1 - (canvasHeightToScale - boundsHeight) / 2;
+    const y2 = y1 + canvasHeightToScale;
+
+    return { scale, x1, x2, y1, y2 };
+  });
+
+  const canvasScale = useComputed(() => canvasBounds.value.scale);
+
+  const svgViewBox = useComputed(
+    () =>
+      `${canvasBounds.value.x1 * canvasBounds.value.scale} ${
+        canvasBounds.value.y1 * canvasBounds.value.scale
+      } ${
+        (canvasBounds.value.x2 - canvasBounds.value.x1) *
+        canvasBounds.value.scale
+      } ${
+        (canvasBounds.value.y2 - canvasBounds.value.y1) *
+        canvasBounds.value.scale
+      }`,
   );
+
+  const graphAxisPath = useComputed(
+    () => `M 0 0 V ${canvasBounds.value.scale} H ${canvasBounds.value.scale}`,
+  );
+
+  const graphSubLinesPath = useComputed(() => {
+    let line = '';
+
+    const { scale, x1, x2, y1, y2 } = canvasBounds.value;
+
+    for (let x = 0; x > x1; x -= 0.25) {
+      line += `M ${x * scale} ${y1 * scale} V ${y2 * scale}`;
+    }
+
+    for (let x = 0; x < x2; x += 0.25) {
+      line += `M ${x * scale} ${y1 * scale} V ${y2 * scale}`;
+    }
+
+    for (let y = 0; y > y1; y -= 0.25) {
+      line += `M ${x1 * scale} ${y * scale} H ${x2 * scale}`;
+    }
+
+    for (let y = 0; y < y2; y += 0.25) {
+      line += `M ${x1 * scale} ${y * scale} H ${x2 * scale}`;
+    }
+
+    return line;
+  });
 
   // A bit hacky, but if the fullPoints is a string, it's an SVG path
   // but we need to flip it so the origin is in the bottom left.
@@ -110,17 +212,13 @@ const Graph: FunctionComponent<Props> = ({
 
   return (
     <div class={styles.graphComponent}>
-      <svg
-        ref={graphSVGRef}
-        class={styles.graphSvg}
-        viewBox="0 0 100 100"
-        style={graphSvgStyle}
-      >
-        <path class={styles.graphAxis} d="M 0 0 V 100 H 100" />
+      <svg ref={graphSVGRef} class={styles.graphSvg} viewBox={svgViewBox}>
+        <path class={styles.graphSublines} d={graphSubLinesPath} />
+        <path class={styles.graphAxis} d={graphAxisPath} />
         <svg
           class={styles.pointsSvg}
-          width="100"
-          height="100"
+          width={canvasScale}
+          height={canvasScale}
           viewBox="0 0 1 1"
         >
           <path
